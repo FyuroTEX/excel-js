c2874b971b114515566eee3a80fee82b63e3634f
FyuroTEX <yurilev4@gmail.com>
2020-05-14 01:25:00 +0300

    add loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,29 +10,44 @@ module.exports = {
     entry: './index.js',
     output: {
         filename: 'bundle.[hash].js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
     },
     resolve: {
         extensions: ['.js'],
         alias: {
             '@': path.resolve(__dirname, 'src'),
-            '@core': path.resolve(__dirname, 'src/core')
-        }
+            '@core': path.resolve(__dirname, 'src/core'),
+        },
     },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: 'index.html'
+            template: 'index.html',
         }),
         new CopyPlugin([
             {
-              from: path.resolve(__dirname, 'src/favicon.ico'),
-              to: path.resolve(__dirname, 'dist')
-            }
-          ]),
+                from: path.resolve(__dirname, 'src/favicon.ico'),
+                to: path.resolve(__dirname, 'dist'),
+            },
+        ]),
         new MiniCssExtractPlugin({
-            filename: 'bundle.[hash].css'
+            filename: 'bundle.[hash].css',
         }),
-        
-    ]
-}
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            },
+        ],
+    },
+};
